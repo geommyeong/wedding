632f5fa013b394a2de76185d905476f2464e694d
geommyeong <hwang-geommyeong@hwang-geommyeong-MacBookPro.local>
2021-04-01 16:43:17 +0900

    router test

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import Card from '@/pages/Card.vue'
-import NotFound from '@/pages/NotFound.vue'
+// import NotFound from '@/pages/NotFound.vue'
 
 const routes = [
   {
@@ -14,10 +14,10 @@ const routes = [
     name: 'Card',
     component: Card,
   },
-  {
-    path: '/:catchAll(.*)',
-    component: NotFound
-  }
+  // {
+  //   path: '/:catchAll(.*)',
+  //   component: NotFound
+  // }
 ];
 
 const router = createRouter({
@@ -26,43 +26,3 @@ const router = createRouter({
 });
 
 export default router;
-
-// // import  from 'vue-router'
-
-
-// const router = new router({
-//   mode: 'history',
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'Home',
-//       components: 'HomeComponent'
-//     },
-//     {
-//       path: '/card',
-//       name: 'WeddingCardFull',
-//       components: 'WeddingCardFull'
-//     }
-//   ]
-// })
-
-// // const routes = [
-// //   {
-// //     path: '/card',
-// //     name: 'card',
-// //     components: {
-// //       default: () => import('../card/Card.vue')
-// //     }
-// //   },
-// //   {
-// //     path: '/',
-// //     name: 'home',
-// //     components: {
-// //       default: () => {
-// //         return import('../')
-// //       }
-// //     }
-// //   }
-// // ]
-
-// // export default router;
